0838f2c8b51957d6aae7c036a43fff7beb021703
Yurbus <frankins636@gmail.com>
2024-04-08 23:13:49 +0300

    main page

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
 "use script"
 
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows());
-    }
-};
+// const isMobile = {
+//     Android: function () {
+//         return navigator.userAgent.match(/Android/i);
+//     },
+//     BlackBerry: function () {
+//         return navigator.userAgent.match(/BlackBerry/i);
+//     },
+//     iOS: function () {
+//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+//     },
+//     Opera: function () {
+//         return navigator.userAgent.match(/Opera Mini/i);
+//     },
+//     Windows: function () {
+//         return navigator.userAgent.match(/IEMobile/i);
+//     },
+//     any: function () {
+//         return (
+//             isMobile.Android() ||
+//             isMobile.BlackBerry() ||
+//             isMobile.iOS() ||
+//             isMobile.Opera() ||
+//             isMobile.Windows());
+//     }
+// };
+
+
 // Меню бурнер
 const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.header__mobile-menu');
+const menuBody = document.querySelector('.header__menu');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
 		document.body.classList.toggle('_lock');
@@ -37,62 +39,62 @@ if (iconMenu) {
 }
 
 // Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if(menuLinks.length > 0) {
-	menuLinks.forEach(menuLink => {
-		menuLink.addEventListener("click", onMenuLinkClick);
-	});
+// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+// if(menuLinks.length > 0) {
+// 	menuLinks.forEach(menuLink => {
+// 		menuLink.addEventListener("click", onMenuLinkClick);
+// 	});
 
-	function onMenuLinkClick(e) {
-		const menuLink = e.target;
-		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+// 	function onMenuLinkClick(e) {
+// 		const menuLink = e.target;
+// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+// 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 		
-			if (iconMenu.classList.contains('_active')) {
-				document.body.classList.remove('_lock');
-				iconMenu.classList.remove('_active');
-				menuBody.classList.remove('_active');
-			}
+// 			if (iconMenu.classList.contains('_active')) {
+// 				document.body.classList.remove('_lock');
+// 				iconMenu.classList.remove('_active');
+// 				menuBody.classList.remove('_active');
+// 			}
 
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth"
-			});
-			e.preventDefault();
-		}
-	}
-}
+// 			window.scrollTo({
+// 				top: gotoBlockValue,
+// 				behavior: "smooth"
+// 			});
+// 			e.preventDefault();
+// 		}
+// 	}
+// }
 
 
 //прикрепление файла в форме
 
-const formImage = document.getElementById('formImage');
-const formPreview = document.getElementById('formPreview');
+// const formImage = document.getElementById('formImage');
+// const formPreview = document.getElementById('formPreview');
 
-formImage.addEventListener('change', () => {
-	uploadFile(formImage.files[0]);
-});
+// formImage.addEventListener('change', () => {
+// 	uploadFile(formImage.files[0]);
+// });
 
-function uploadFile(file) {
-	if (!['image/jpeg', 'image/png', 'image/gif', 'image/pdf'].includes(file.type)) {
-		alert('Разрешены только изображения!');
-		formImage.value ='';
-		return;
-	}
-	if (file.size > 2 * 1024 * 1024) {
-		alert('Файл должен быть менее 2 МБ.');
-		return;
-	}
-	var reader = new FileReader();
-	reader.onload = function (e) {
-		formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-	};
-	reader.onerror = function (e) {
-		alert('Ошибка');
-	};
-	reader.readAsDataURL(file);
-}
+// function uploadFile(file) {
+// 	if (!['image/jpeg', 'image/png', 'image/gif', 'image/pdf'].includes(file.type)) {
+// 		alert('Разрешены только изображения!');
+// 		formImage.value ='';
+// 		return;
+// 	}
+// 	if (file.size > 2 * 1024 * 1024) {
+// 		alert('Файл должен быть менее 2 МБ.');
+// 		return;
+// 	}
+// 	var reader = new FileReader();
+// 	reader.onload = function (e) {
+// 		formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+// 	};
+// 	reader.onerror = function (e) {
+// 		alert('Ошибка');
+// 	};
+// 	reader.readAsDataURL(file);
+// }
 
 
 //style main-bg
@@ -107,16 +109,3 @@ function uploadFile(file) {
 
 
 
-// $('#form').submit(function(){
-// 	$.ajax({
-// 		type:"POST",
-// 		url: "mail.php",
-// 		data: $(this).serialize()
-// 	}).done(function() {
-// 	alert("Спасибо что обратились ко мне!");
-// 	setTimeout(function() {
-// 		$.magnificPopup.close();
-// 	}, 1000);
-// 	});
-// 	return false;
-// });
